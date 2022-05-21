505c24199b12b6a41b5b0bfffb9b9d83c85e58fb
OlgaGre <grechanyuko@gmail.com>
2022-05-21 16:25:06 +0300

    feat:05 done

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 let arrCss = [];
 let pathDundle = __dirname + '/project-dist/bundle.css'
-console.log(pathDundle)
+
 
 // очистка doudle.css
 fs.unlink(pathDundle, (err) => {
@@ -11,22 +11,23 @@ fs.unlink(pathDundle, (err) => {
 });
 // поиск файлов css
 fs.readdir(__dirname + '/styles', (err, files) => {
-        if (err)
-            console.log(err);
-        else {
-            console.log(files)
-            for (let i = 0; i < files.length; i++) {
-                if (files[i].split('.')[1] == 'css') {
-                    arrCss.push(files[i])
-                    readFile(files[i])
-                }
+    if (err)
+        console.log(err);
+    else {
+
+        for (let i = 0; i < files.length; i++) {
+            if (files[i].split('.')[1] == 'css') {
+                arrCss.push(files[i])
+                readFile(files[i])
             }
         }
-        console.log(arrCss)
-    }) ///.(?=\.)/g   node 05-merge-styles  05-merge-styles\project-dist\index.html
+    }
+
+})
+
 function readFile(file) {
     fs.readFile(__dirname + '/styles/' + file, 'utf8', (error, data) => {
-        // console.log(data)
+
         writeFile(data)
     });
 }
